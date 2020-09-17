6a37ce472ba86643f6dbeebb7d0afa1239c24b74
Alex Neugebauer <alex.neugebauer@fau.de>
2020-09-17 17:10:46 +0200

    Update startWorkflowExecution.py

--- startWorkflowExecution.py
+++ startWorkflowExecution.py
@@ -10,5 +10,5 @@ l = 0
 
 for filename in data:
     os.system(
-        "python3 workflow.py --normal_forward_reads UKER" + str(l) + "_R1_merged.fastq.gz --regions Galaxy13-[capture_targets_chr5_12_17.bed].bed --normal_reverse_reads UKER" + str(l) + "_R2_merged.fastq.gz --tumor_forward_reads UKER26_R1_merged.fastq.gz  --tumor_reverse_reads UKER26_R2_merged.fastq.gz --galaxy_url https://usegalaxy.eu --api_key dMIAGYNtHZrj9q52ZqYqr0xZT68qIDY --workflow_id_override=f3be319d63e1912f")
+        "python3 run_workflow_panel_main.py --normal_forward_reads UKER" + str(l) + "_R1_merged.fastq.gz --regions Galaxy13-[capture_targets_chr5_12_17.bed].bed --normal_reverse_reads UKER" + str(l) + "_R2_merged.fastq.gz --tumor_forward_reads UKER26_R1_merged.fastq.gz  --tumor_reverse_reads UKER26_R2_merged.fastq.gz --galaxy_url https://usegalaxy.eu --api_key dMIAGYNtHZrj9q52ZqYqr0xZT68qIDY --workflow_id_override=f3be319d63e1912f")
     l += 1
