11e7300526e27215ade335e0e5c8597aa7e7b1fa
Alex Neugebauer <alex.neugebauer@fau.de>
2020-09-18 18:00:09 +0200

    Update startWorkflowExecution.py

--- startWorkflowExecution.py
+++ startWorkflowExecution.py
@@ -17,8 +17,8 @@ l = 26
 
 gi = galaxy.GalaxyInstance(url='http://srv-ap-omics1.srv.uk-erlangen.de/', key='64b1a4440d46af31d546df70cc5db50d')
 hl = gi.histories.get_histories()
-#hh = gi.histories.show_history(history_id = hl, contents=True, deleted=None, visible=True, details=True, types=None)
-print(hl)
+hh = gi.histories.show_history(history_id = 'be8c8ac3dbd1dd54', contents=True, deleted=None, visible=True, details=True, types=None)
+
 
 def get_datasets(self, limit=500, offset=0):
     params = {
@@ -69,8 +69,8 @@ for filename in data:
             l) + "_R1_merged.fastq.gz --dbsnp_records abafdf086c375ee5 --api_key 64b1a4440d46af31d546df70cc5db50d --galaxy_url http://srv-ap-omics1.srv.uk-erlangen.de/ --workflow_id_override=a0c15f4d91084599 --new_history_name UKER" + str(
             l))
     
-   # data_info = hh.show_history(history_id='UKER' + str(l), contents=True, deleted=None, visible=True, details=True, types=None)
-   # print(data_info)
+    data_info = hh.show_history(history_id='UKER' + str(l), contents=True, deleted=None, visible=True, details=True, types=None)
+    print(data_info)
     
     os.system(
         "python3 run_workflow_panel_variant_annotation.py --variants_input --dbsnp_annotations abafdf086c375ee5 --cancerhotspots_data__bed_ c344e7e8c8cc61aa --civic_data__bed_ 3031e83883b39f24 --cgi_biomarkers__bed_ 8aab8fda5bfd5997 --api_key 64b1a4440d46af31d546df70cc5db50d --galaxy_url  http://srv-ap-omics1.srv.uk-erlangen.de/ --workflow_id_override=86cf1d3beeec9f1c --new_history_name UKER" + str(
