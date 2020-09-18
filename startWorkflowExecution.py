de1ad6ddaa7cb5fc9a65ae82458e2376080a485a
Alex Neugebauer <alex.neugebauer@fau.de>
2020-09-18 11:01:09 +0200

    Update startWorkflowExecution.py

--- startWorkflowExecution.py
+++ startWorkflowExecution.py
@@ -9,6 +9,6 @@ data = glob.glob('/Volumes/Base/PycharmProjects/MTB/*_R1_merged.fastq.gz')
 l = 0
 
 for filename in data:
-    os.system(
-        "python3 run_workflow_panel_main.py --normal_forward_reads UKER" + str(l) + "_R1_merged.fastq.gz --regions Galaxy13-[capture_targets_chr5_12_17.bed].bed --normal_reverse_reads UKER" + str(l) + "_R2_merged.fastq.gz --tumor_forward_reads UKER26_R1_merged.fastq.gz  --tumor_reverse_reads UKER26_R2_merged.fastq.gz --galaxy_url https://usegalaxy.eu --api_key 64b1a4440d46af31d546df70cc5db50d --workflow_id_override=132016f833b57406")
+    os.system()
+        "python3 run_workflow_panel_main.py --forward_reads UKER" + str(l) + "_R1_merged.fastq.gz --reverse_reads UKER" + str(l) + "_R1_merged.fastq.gz --dbsnp_records abafdf086c375ee5 --api_key 64b1a4440d46af31d546df70cc5db50d --galaxy_url http://srv-ap-omics1.srv.uk-erlangen.de/ --workflow_id_override=a0c15f4d91084599 --new_history_name UKER" + str(l) ")
     l += 1
