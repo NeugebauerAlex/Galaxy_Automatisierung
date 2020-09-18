4464e6720352dd884ca51645ffc1b2be7c4720c4
Alex Neugebauer <alex.neugebauer@fau.de>
2020-09-18 18:02:01 +0200

    Update startWorkflowExecution.py

--- startWorkflowExecution.py
+++ startWorkflowExecution.py
@@ -69,8 +69,9 @@ for filename in data:
             l) + "_R1_merged.fastq.gz --dbsnp_records abafdf086c375ee5 --api_key 64b1a4440d46af31d546df70cc5db50d --galaxy_url http://srv-ap-omics1.srv.uk-erlangen.de/ --workflow_id_override=a0c15f4d91084599 --new_history_name UKER" + str(
             l))
     
-    data_info = hh.show_history(history_id='UKER' + str(l), contents=True, deleted=None, visible=True, details=True, types=None)
-    print(data_info)
+    print(hl)
+    #data_info = hh.show_history(history_id='UKER' + str(l), contents=True, deleted=None, visible=True, details=True, types=None)
+    #print(data_info)
     
     os.system(
         "python3 run_workflow_panel_variant_annotation.py --variants_input --dbsnp_annotations abafdf086c375ee5 --cancerhotspots_data__bed_ c344e7e8c8cc61aa --civic_data__bed_ 3031e83883b39f24 --cgi_biomarkers__bed_ 8aab8fda5bfd5997 --api_key 64b1a4440d46af31d546df70cc5db50d --galaxy_url  http://srv-ap-omics1.srv.uk-erlangen.de/ --workflow_id_override=86cf1d3beeec9f1c --new_history_name UKER" + str(
