954682c054038ea8b171ee42e2eb0499761b7d75
Alex Neugebauer <alex.neugebauer@fau.de>
2020-09-18 15:21:51 +0200

    Update run.bash

--- run.bash
+++ run.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
 # -*- coding:utf-8 -*-
 
-#python3 combinefastqFiles.py
+python combinefastqFiley.py
 
 python startWorkflowExecution.py
 
-#python3 workflow.py --forward_reads UKER26_R1_merged.fastq --reverse_reads UKER26_R2_merged.fastq --hg19_canonical_chromosomes hg19_canonical_contigs.bed --dbsnp_records dbsnp.b147.20160601.tidy.vcf.gz --galaxy_url http://srv-ap-omics1.srv.uk-erlangen.de --api_key 6041f44c86bc4db709c7c5650f3405bd --workflow_id_override=fb85969571388350
 
