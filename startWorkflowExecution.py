8d0a532715a28b830b88a2bc24f8e1ec156d5511
Alex Neugebauer <alex.neugebauer@fau.de>
2020-09-18 13:26:31 +0200

    Update startWorkflowExecution.py

--- startWorkflowExecution.py
+++ startWorkflowExecution.py
@@ -3,7 +3,20 @@
 
 import os
 import glob
-import bioblend
+
+import true as true
+from bioblend import galaxy
+
+gi = galaxy.GalaxyInstance(url='http://srv-ap-omics1.srv.uk-erlangen.de/', key='64b1a4440d46af31d546df70cc5db50d')
+hl = gi.histories.get_histories()
+
+
+def delete_history(self, history_id, purge=true):
+    payload = {}
+    if purge is True:
+        payload['purge'] = purge
+    return self._delte(payload=payload, id=history_id)
+
 
 data = glob.glob('/Volumes/Base/PycharmProjects/MTB/*_R1_merged.fastq.gz')
 
@@ -11,12 +24,19 @@ l = 0
 
 for filename in data:
     os.system(
-        "python3 run_workflow_panel_main.py --forward_reads UKER" + str(l) + "_R1_merged.fastq.gz --reverse_reads UKER" + str(l) + "_R1_merged.fastq.gz --dbsnp_records abafdf086c375ee5 --api_key 64b1a4440d46af31d546df70cc5db50d --galaxy_url http://srv-ap-omics1.srv.uk-erlangen.de/ --workflow_id_override=a0c15f4d91084599 --new_history_name UKER" + str(l))
+        "python3 run_workflow_panel_main.py --forward_reads UKER" + str(
+            l) + "_R1_merged.fastq.gz --reverse_reads UKER" + str(
+            l) + "_R1_merged.fastq.gz --dbsnp_records abafdf086c375ee5 --api_key 64b1a4440d46af31d546df70cc5db50d --galaxy_url http://srv-ap-omics1.srv.uk-erlangen.de/ --workflow_id_override=a0c15f4d91084599 --new_history_name UKER" + str(
+            l))
 
     os.system(
-        "python3 run_workflow_panel_variant_annotation.py --variants_input --dbsnp_annotations abafdf086c375ee5 --cancerhotspots_data__bed_ c344e7e8c8cc61aa --civic_data__bed_ 3031e83883b39f24 --cgi_biomarkers__bed_ 8aab8fda5bfd5997 --api_key 64b1a4440d46af31d546df70cc5db50d --galaxy_url  http://srv-ap-omics1.srv.uk-erlangen.de/ --workflow_id_override=86cf1d3beeec9f1c --new_history_name UKER" + str(l))
+        "python3 run_workflow_panel_variant_annotation.py --variants_input --dbsnp_annotations abafdf086c375ee5 --cancerhotspots_data__bed_ c344e7e8c8cc61aa --civic_data__bed_ 3031e83883b39f24 --cgi_biomarkers__bed_ 8aab8fda5bfd5997 --api_key 64b1a4440d46af31d546df70cc5db50d --galaxy_url  http://srv-ap-omics1.srv.uk-erlangen.de/ --workflow_id_override=86cf1d3beeec9f1c --new_history_name UKER" + str(
+            l))
 
     os.system(
-        "python3 run_workflow_panel_report_variant.py  --sample_identifier UKER" +str(l) + "--gemini_db_of_variants --uniprot_annotated_cancer_genes  --cgi_listed_genes 8aab8fda5bfd5997  --civic_genes d513c0e53ab96eac --api_key 64b1a4440d46af31d546df70cc5db50d --galaxy_url http://srv-ap-omics1.srv.uk-erlangen.de/ --workflow_id_override=8c959c9304a2bc4b")
+        "python3 run_workflow_panel_report_variant.py  --sample_identifier UKER" + str(
+            l) + "--gemini_db_of_variants --uniprot_annotated_cancer_genes  --cgi_listed_genes 8aab8fda5bfd5997  --civic_genes d513c0e53ab96eac --api_key 64b1a4440d46af31d546df70cc5db50d --galaxy_url http://srv-ap-omics1.srv.uk-erlangen.de/ --workflow_id_override=8c959c9304a2bc4b")
+
+    delete_history("UKER" + str(l), purge=true)
 
     l += 1
