6073c75927d72880a309a38ab38a199cca2d9cba
Alex Neugebauer <alex.neugebauer@fau.de>
2020-10-22 12:21:26 +0200

    Update startWorkflowExecution.py

--- startWorkflowExecution.py
+++ startWorkflowExecution.py
@@ -130,8 +130,8 @@ for filename in data:
    # gi.histories.download_dataset(history_id= li, dataset_id='', file_path='/home/neugebax/UKER' + str(l) + 'report', use_default_filename=True)
 
     
-    data = gi.histories.show_history(history_id=li_element, contents=False)
-    print(data)
+    not_data = gi.histories.show_history(history_id=li_element, contents=False)
+    print(not_data)
 
     #gi.histories.delete_history(history_id=li_element, purge=True)
     #l += 1
