843f94d514da1f2bac56ffe47ae806f9683bc452
Alex Neugebauer <alex.neugebauer@fau.de>
2020-10-26 16:12:17 +0100

    Update startWorkflowExecution.py

--- startWorkflowExecution.py
+++ startWorkflowExecution.py
@@ -135,6 +135,8 @@ for filename in data:
     #l += 1
     time.sleep(t)
     
+    find_id = gi.histories.get_datasets(limit=500, offset=0)
+    print(find_id)
     # Downloaden aller Daten funktioniert, Dataset_id eingrenzen fehlt?
     gi.histories.download_dataset(history_id=li_element, dataset_id=li_element, file_path='/home/neugebax/Download', use_default_filename=True)
 #not_data = gi.histories.show_matching_datasets(history_id=li_element, name_filter='[MAF_report_of_variants].tabular')
