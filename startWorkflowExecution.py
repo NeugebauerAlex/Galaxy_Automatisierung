e835b1fecf10d31510144d8a30e2a8ad6f5f8528
Alex Neugebauer <alex.neugebauer@fau.de>
2020-10-26 12:50:48 +0100

    Update startWorkflowExecution.py

--- startWorkflowExecution.py
+++ startWorkflowExecution.py
@@ -104,7 +104,7 @@ for filename in data:
         "python3 run_workflow_panel_main.py --sample_name UKER" +str(l) +"run --forward_reads UKER" + str(
             l) + "_R1_merged.fastq.gz --reverse_reads UKER" + str(
             l) + "_R2_merged.fastq.gz --dbsnp_records abafdf086c375ee5 --api_key 64b1a4440d46af31d546df70cc5db50d --galaxy_url http://srv-ap-omics1.srv.uk-erlangen.de/ --workflow_id_override=319886f37b7797fe --new_history_name UKER" + str(
-            l))"
+            l))
     
 
     hh = gi.histories.get_histories(history_id=None, name="UKER" +str(l), deleted=False)
