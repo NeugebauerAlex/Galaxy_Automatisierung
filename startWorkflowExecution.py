bb5cd31f3a8e787e8ca215b78b246ec59dd9e2f6
Alex Neugebauer <alex.neugebauer@fau.de>
2020-10-26 16:58:41 +0100

    Update startWorkflowExecution.py

--- startWorkflowExecution.py
+++ startWorkflowExecution.py
@@ -15,7 +15,7 @@ from bioblend import galaxy
 from bioblend.galaxy.client import Client
 
 l = 40
-t = 300
+t = 180
 
 gi = galaxy.GalaxyInstance(url='http://srv-ap-omics1.srv.uk-erlangen.de/', key='64b1a4440d46af31d546df70cc5db50d')
 hl = gi.histories.get_histories()
