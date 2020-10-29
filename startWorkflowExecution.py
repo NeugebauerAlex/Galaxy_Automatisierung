188b461818960cfb9768fdf15033a6915c093a80
Alex Neugebauer <alex.neugebauer@fau.de>
2020-10-29 15:18:12 +0100

    Update startWorkflowExecution.py

--- startWorkflowExecution.py
+++ startWorkflowExecution.py
@@ -142,7 +142,7 @@ for filename in data:
  # not_data = gi.histories.show_matching_datasets(history_id=li_element, name_filter='Galaxy11-[FastQC_on_data_2__RawData]')
     no_data = gi.histories.show_history(li_element, contents=False)
     no_data_string = str(no_data)
-    find_id = data_set_id['state_ids']['ok']
+    find_id = no_data_string['state_ids']['ok']
     print(find_id)
     print(find_id[1])
 
