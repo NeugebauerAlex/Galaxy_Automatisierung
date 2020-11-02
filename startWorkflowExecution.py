6de96d8462ed26e9a685a5a864eb1914511582ff
Alex Neugebauer <alex.neugebauer@fau.de>
2020-11-02 18:02:06 +0100

    Update startWorkflowExecution.py

--- startWorkflowExecution.py
+++ startWorkflowExecution.py
@@ -161,9 +161,11 @@ for filename in data:
     time.sleep(m)
 
     # Starte dritten Workflow
+    input_zwei = (find_id_safe_zwei)
+    
     os.system(
         "python3 run_workflow_panel_report_variant.py  --sample_identifier UKER" + str(
-           l) + "--gemini_db_of_variants --uniprot_annotated_cancer_genes 07acaf50ebe1f533 --cgi_listed_genes 8aab8fda5bfd5997  --civic_genes d513c0e53ab96eac --api_key 64b1a4440d46af31d546df70cc5db50d --galaxy_url http://srv-ap-omics1.srv.uk-erlangen.de/ --workflow_id_override=8c959c9304a2bc4b")
+           l) + "--gemini_db_of_variants %s --uniprot_annotated_cancer_genes 07acaf50ebe1f533 --cgi_listed_genes 8aab8fda5bfd5997  --civic_genes d513c0e53ab96eac --api_key 64b1a4440d46af31d546df70cc5db50d --galaxy_url http://srv-ap-omics1.srv.uk-erlangen.de/ --workflow_id_override=8c959c9304a2bc4b"%input_zwei)
 
     # History ID des dritten Workflows herauskriegen
     ll = gi.histories.get_histories(history_id=None, name="UKER" +str(l), deleted=False)
