8985b23be7150cb92414fefdfa74242ca406f282
Alex Neugebauer <alex.neugebauer@fau.de>
2020-11-09 13:35:42 +0100

    Update startWorkflowExecution.py

--- startWorkflowExecution.py
+++ startWorkflowExecution.py
@@ -168,14 +168,14 @@ for filename in data:
     os.system(
         "python3 run_workflow_panel_report_variant.py  --sample_identifier UKER --gemini_db_of_variants %s --uniprot_annotated_cancer_genes 07acaf50ebe1f533 --cgi_listed_genes 8aab8fda5bfd5997 --civic_genes c9213a53ccefcc61 --api_key 64b1a4440d46af31d546df70cc5db50d --galaxy_url http://srv-ap-omics1.srv.uk-erlangen.de/ --workflow_id_override=8c959c9304a2bc4b --new_history_name UKER_DREI"%input)
 
+    # Warte bis Durchgang vollzogen ist 
+    time.sleep(o)
+
     # History ID des dritten Workflows herauskriegen
     ll = gi.histories.get_histories(history_id=None, name="UKER_DREI", deleted=False)
     bi = [item.get('id') for item in ll]
     bi_element = bi[0]
     bi_element_string = str(bi_element)
-
-    # Warte bis Durchgang vollzogen ist 
-    time.sleep(0)
 
     # Dataset ID finden und herunterladen
     data_set_drei = gi.histories.show_history(bi_element, contents=False)
@@ -188,9 +188,6 @@ for filename in data:
     gi.histories.download_dataset(history_id=bi_element, dataset_id=maf_report, file_path='/home/neugebax/Download', use_default_filename=True)
     gi.histories.download_dataset(history_id=bi_element, dataset_id=annotation_report, file_path='/home/neugebax/Download', use_default_filename=True)
     gi.histories.download_dataset(history_id=bi_element, dataset_id=gene_report, file_path='/home/neugebax/Download', use_default_filename=True)
-
-
-
 
     #Warte kurz bis löschen von Histories losgeht
     time.sleep(m)
