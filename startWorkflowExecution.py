6848d482b491a5abc337fe3bd5e26e4c36cb1693
Alex Neugebauer <alex.neugebauer@fau.de>
2020-11-09 18:24:33 +0100

    Update startWorkflowExecution.py

--- startWorkflowExecution.py
+++ startWorkflowExecution.py
@@ -152,7 +152,7 @@ for filename in data:
     data_set_zwei = gi.histories.show_history(zi_element, contents=False)
     find_id_zwei = data_set_zwei['state_ids']['ok']
     find_id_safe_zwei = find_id_zwei[12] 
-    print(find_id_safe)
+    print(find_id_safe_zwei)
 
     # Lade das Dataset herunter
     gi.histories.download_dataset(history_id=zi_element, dataset_id=find_id_safe_zwei, file_path='/home/neugebax/Download/UKER40', use_default_filename=True)
